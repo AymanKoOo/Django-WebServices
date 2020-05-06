57f15895019a9aefe4ce539794db54b300a66ea3
Ayman <aymannaser14@gmail.com>
2020-05-06 04:11:01 +0200

    sql server

--- webapp/views.py
+++ webapp/views.py
@@ -2,41 +2,72 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
-
+import pyodbc
 # Create your views here.
 import json
 from webapp.models import picM
 
+#<<<<Connect SQL SERVER DB>>>>>>
+
+
+def conection():
+    conn = pyodbc.connect(
+        "Driver={SQL Server Native Client 11.0};"
+        "Server=DESKTOP-MPMVGIB\SQLEXPRESS;"
+        "Database=NuzhaTec;"
+        "Trusted_Connection=yes;"
+        )
+    return conn
+    
 #<<<<<<<<<<<<<<<<<<<<<<<<default route>>>>>>>>>>>>>>>>#
 def index(request):
-    response = json.dumps([{}])
+  
+    conn=conection();
+    cursor = conn.cursor()
+    cursor.execute("select * from Nzha_ServicePhoto")
+    for row in cursor:
+        response = json.dumps(f'row = {row}')
+        print(json.dumps(f'row = {row}'))
+    conn.close()
     return HttpResponse(response, content_type='text/json')
 
 
 #<<<<<<<<<<<<<<<<<<<<<<<<GET PIC Details >>>>>>>>>>>>>>>>#
 def get_pic(request, pic_id):
     if request.method == 'GET':
-        try:
-            picOb = picM.objects.get(idd=pic_id)
-            response = json.dumps([{ 'idd': picOb.idd,'name':picOb.name,'pic':picOb.pic}])
-            
-        except:
-            response = json.dumps([{ 'Error': 'No Id with that name'}])
+        
+             conn=conection();
+             cursor = conn.cursor()
+             cursor.execute("select * from Nzha_ServicePhoto where ID="+pic_id)
+             for row in cursor:
+               response = json.dumps(f'row = {row}')
+               print(json.dumps(f'row = {row}'))
+         
+             conn.close()   
+                  
     return HttpResponse(response, content_type='text/json')
 
 #<<<<<<<<<<<<<<<<<<<<<<<<POst PIC Details >>>>>>>>>>>>>>>>#
 @api_view(['POST'])
 def addId(request):
     if request.method == 'POST':
-        payload = json.loads(request.body)
-        picName=payload['name']
-        picid=payload['idd']
-        picUrl=payload['pic']
-        
-        picOb = picM(idd=picid,name=picName,pic=picUrl)
-        try:
-            picOb.save()
-            response = json.dumps([{ 'Success': 'pic added successfully!'}])
-        except:
-            response = json.dumps([{ 'Error': 'pic could not be added!'}])
+
+            payload = json.loads(request.body)
+
+            ID=payload['ID']
+            photoFilePath=payload['photoFilePath']
+            Nzha_Service_ID=payload['Nzha_Service_ID']
+            Nzha_User_ID=payload['Nzha_User_ID']
+            filterString=payload['filterString']
+
+            conn = conection()
+            cursor = conn.cursor()
+            sql = """\
+            EXEC SP_ServicePhoto @ID=?,@photoFilePath=?,@Nzha_Service_ID=?,@Nzha_User_ID=?,@filterString=?
+            """
+            params =  (ID, photoFilePath,Nzha_Service_ID,Nzha_User_ID,filterString)
+            cursor.execute(sql, params)
+            conn.commit()
+            response = json.dumps(f'row = {"done"}')
+            
     return HttpResponse(response, content_type='text/json')
